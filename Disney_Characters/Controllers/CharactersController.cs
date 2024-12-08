bb8a21a175ebb7600a356cefc4e012dd539ad374
BatuAksut <batuhanaksut@hotmail.com>
2024-12-08 14:15:49 +0300

    feat(api): add getall method to fetch all characters

---
+++ Disney_Characters/Controllers/CharactersController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace Disney_Characters.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharactersController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+
+        public CharactersController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _httpClient.GetAsync("https://api.disneyapi.dev/character");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                return Content(content, "application/json");
+            }
+
+            return BadRequest("API çağrısı başarısız oldu.");
+        }
+    }
+}
